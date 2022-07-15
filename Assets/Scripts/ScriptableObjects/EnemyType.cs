f0d482791822a3d41b844b1f2a002367418f2785
Johannes Kloeckner <johannes.kloeckner@fkie.fraunhofer.de>
2022-07-15 22:10:31 +0200

    Add basic enemy prefabs (chase collider labeled "Player" when its seen); Add enemy types scriptable objects; Add test scene for enemy AI

---
+++ Assets/Scripts/ScriptableObjects/EnemyType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyType", menuName = "EnemyType/Generic", order = 1)]
+public class EnemyType : ScriptableObject
+{
+    public string enemyName;
+    public float preferredDistance;
+    public float visionRange = 20f;
+
+    //public Weapon usedWeapon
+}
