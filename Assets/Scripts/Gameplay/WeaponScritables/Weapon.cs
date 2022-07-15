eb8c63dedd7113ad0aaaaf28c55745dab2cf7c1b
nackor <nackor91@gmail.com>
2022-07-15 12:07:19 -0700

    made some test scritables

--- Assets/Scripts/Gameplay/WeaponScritables/Weapon.cs
+++ Assets/Scripts/Gameplay/WeaponScritables/Weapon.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Weapon", menuName = "ScriptableObjects/Weapon", order = 50)]
 public class Weapon : ScriptableObject
 {
+    //name of the weapon
     public string weaponName;
+    //damage of the weapon
     public int damage;
+    //max/starting ammo
     public int maxAmmo;
-    public Vector3[] spawnPoints;
+    //whether or not this is a hitscan usuage
+    public bool hitScan;
+    //how much ammow goes down when firing
+    public int ammoUsuage;
+
 }
