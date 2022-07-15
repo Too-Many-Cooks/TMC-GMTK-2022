6b303a783a0e9637073fdca370284fe6ab7db445
Daniel Kelly <dlpkelly@hotmail.com>
2022-07-15 15:39:19 -0400

    Initial work on die display

--- Assets/Scripts/Dice/DieDisplay.cs
+++ Assets/Scripts/Dice/DieDisplay.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class DieDisplay : MonoBehaviour
 {
     public Die die;
+    public GameObject d4Prefab;
+    public GameObject d6Prefab;
+    public GameObject d8Prefab;
+    public GameObject d10Prefab;
+    public GameObject d12Prefab;
+
     private GameObject dieObject;
 
     // Start is called before the first frame update
@@ -12,9 +18,20 @@ public class DieDisplay : MonoBehaviour
     {
         switch(die.NumberOfSides)
         {
-            case 6:
-                dieObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                dieObject.transform.parent = transform;
+            case >= 12:
+                dieObject = GameObject.Instantiate(d12Prefab, transform);
+                break;
+            case >= 10:
+                dieObject = GameObject.Instantiate(d10Prefab, transform);
+                break;
+            case >= 8:
+                dieObject = GameObject.Instantiate(d8Prefab, transform);
+                break;
+            case >= 6:
+                dieObject = GameObject.Instantiate(d6Prefab, transform);
+                break;
+            case >= 4:
+                dieObject = GameObject.Instantiate(d4Prefab, transform);
                 break;
             default:
                 dieObject = null;
