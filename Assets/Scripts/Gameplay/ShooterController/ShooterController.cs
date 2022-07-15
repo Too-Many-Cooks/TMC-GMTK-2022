67e47bed9726e33584e47e6e44120388a0d29cee
nackor <nackor91@gmail.com>
2022-07-15 15:01:57 -0700

    add debug log

--- Assets/Scripts/Gameplay/ShooterController/ShooterController.cs
+++ Assets/Scripts/Gameplay/ShooterController/ShooterController.cs
@@ -72,19 +72,17 @@ public class ShooterController : MonoBehaviour
         if (CurrentWeapon.hitScan)
         {
             // Create a vector at the center of our camera's viewport
-            Vector3 rayOrigin = _camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
-
             // Declare a raycast hit to store information about what our raycast has hit
             RaycastHit hit;
             LayerMask enemyLayerMask = LayerMask.GetMask("Enemy");
             Physics.Raycast(worldPos, _camera.transform.forward, out hit, CurrentWeapon.weaponRange, enemyLayerMask);
-            Debug.Log(hit.transform);
-
-
+            Debug.Log("hit enemy");
         }
         else
         {
             //do projectile thingies.
+            GameObject ball = Instantiate(CurrentWeapon.bullet, transform.position, transform.rotation);
+            ball.GetComponent<Rigidbody>().AddForce(Vector3.forward * 100);
         }
     }
 
